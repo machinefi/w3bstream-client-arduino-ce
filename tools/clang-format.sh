095412bd4f2ad18ebd92f4863b07f4c8a9b574ef
as-iotex <82106612+as-iotex@users.noreply.github.com>
2023-09-18 11:49:04 +0100

    ci(code formatting): exclude examples from clang-format

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
- find ../src -path "../src/extern" -prune -o -path "../src/tinycrypt" -prune -o -path "../src/include/tinycrypt" -prune -o \( -name "*.cpp" -o -name "*.c" -o -name "*.h" -o -name "*.ino" \) | grep -v "../src/tinycrypt" | grep -v "../src/include/tinycrypt" | xargs -I {} clang-format -style=file -i {}
-
+find ../src -path "../src/extern" -prune -o \( -name "*.cpp" -o -name "*.c" -o -name "*.h" -o -name "*.ino" \) | xargs -I {} clang-format -style=file -i {}
